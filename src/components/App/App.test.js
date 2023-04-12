edbb5f31985a76da6be2b797c796d6b1f92f7b93
Wendel Costa <wendel@wendelcosta.com>
2023-04-12 20:02:30 +1000

    Fixed unit test

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -3,6 +3,6 @@ import App from '.'
 
 test('renders learn react link', () => {
 	render(<App />)
-	const linkElement = screen.getByText(/learn react/i)
+	const linkElement = screen.getByText(/Flickr Search/i)
 	expect(linkElement).toBeInTheDocument()
 })
